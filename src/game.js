2921f782e93875ad0235873e7516d017188f41e5
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2019-12-27 15:24:00 +0300

    add rotation

--- src/game.js
+++ src/game.js
@@ -31,10 +31,10 @@ export default class Game {
 		x: 0,
 		y: 0,
 		blocks: [
-		[0,1,0],
-		[1,1,1],
-		[0,0,0]
-		]
+			[0,1,0],
+			[1,1,1],
+			[0,0,0]
+		],
 	};
 
 	//functions movement of the active figure
@@ -61,6 +61,40 @@ export default class Game {
 		}
 	}
 
+	//figure rotation
+	rotatePiece() {
+		this.rotateBlocks();
+
+		if (this.hasCollision()) {
+			this.rotateBlocks(false);
+		}
+	}
+
+	rotateBlocks(clockwise = true) {
+		const blocks = this.activePiece.blocks;
+		const length = blocks.length;
+		const x = Math.floor(length / 2);
+		const y = length - 1;
+
+		for (let i = 0; i < x; i++) {
+			for (let j = i; j < y - i; j++) {
+				const temp = blocks[i][j];
+
+				if (clockwise) {
+					blocks[i][j] = blocks[y - j][i];
+					blocks[y - j][i] = blocks[y - i][y - j];
+					blocks[y - i][y - j] = blocks[j][y - i];
+					blocks[j][y - i] = temp;
+				} 	else {
+					blocks[i][j] = blocks[j][y -i];
+					blocks[j][y - i] = blocks[y - i][y - j];
+					blocks[y - i][y - j] = blocks[y - j][i];
+					blocks[y - j][i] = temp;
+				}					
+			}
+		}
+	}
+
 	//Checking a figure out of the field and collisions with other figures
 	hasCollision() {
 		const {y: pieceY, x: pieceX, blocks} = this.activePiece;
@@ -69,7 +103,7 @@ export default class Game {
 			for (let x = 0; x < blocks[y].length; x++) {
 				if (blocks[y][x] && 
 					((this.playField[pieceY + y] === undefined || this.playField[pieceY + y][pieceX + x] === undefined) ||
-					this.playField[pieceY + y][pieceX + x])) {
+						this.playField[pieceY + y][pieceX + x])) {
 					return true;
 			}
 		}
